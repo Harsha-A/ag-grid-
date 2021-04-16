7b7ae04366fe916713af1ec66169074d33ff8f99
Harsha Anantharamu <45288017+Harsha-A@users.noreply.github.com>
2021-04-16 14:18:20 +0530

    Initial changes

---
+++ ag-grid/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {AgGridModule} from 'ag-grid-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
